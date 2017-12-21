94b88edd3f5118aa78e445f9b2a01c31c63102a4
Troy Goode <troygoode@gmail.com>
2017-12-21 09:23:26 -0800

    only show rolls with a message on homepage

--- queries/fetch-recent-entries.js
+++ queries/fetch-recent-entries.js
@@ -1,7 +1,7 @@
 module.exports = async (pg) => {
   const entryResults = await pg.query({
     name: 'fetch-recent-entries',
-    text: 'SELECT message, roll, result, created_at, log_id FROM entries ORDER BY created_at DESC LIMIT 10;',
+    text: 'SELECT message, roll, result, created_at, log_id FROM entries WHERE CHAR_LENGTH(message) > 0  ORDER BY created_at DESC LIMIT 10;',
     values: []
   })
   return entryResults.rows
